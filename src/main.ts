c29221c5823b5bbac757340d57607c41d3557eb0
lebraz1998 <jawadfadel62@gmail.com>
2022-12-21 22:19:41 +0200

    fix port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || 3000;
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
